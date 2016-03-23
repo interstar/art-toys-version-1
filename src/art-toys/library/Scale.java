c8b4e4a51e4ca8d02cd81e71208b2932577464b0
phil jones <interstar@gmail.com>
2016-03-23 15:20:50 -0300

    added transpose value to scale

--- src/art-toys/library/Scale.java
+++ src/art-toys/library/Scale.java
@@ -3,12 +3,18 @@ package arttoys.core;
 public class Scale {
     int[] scale;
 
+    int transpose=0;
+
     public Scale(int[] s) {  scale = s;  }
 
     public int transform(int note) {
       int n = note % 12;
       int c = 0;
       while (scale[(n+c)%12] == 0) { c = c + 1; }
-      return note + c;
+      return note + c + transpose;
     }
+
+    public void setTranspose(int t) { transpose = t; }
+    public int getTranspose() { return transpose; }
+    public void shift(int x) { transpose = (transpose+x)%12; }
 }
